916c885018bf0ec4722c4d195ce7eb71d40890a5
luiz-a <luizsantos.2782@gmail.com>
2022-06-18 20:53:54 -0300

    Tela inicial de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StartMenuComponent } from './components/start-menu/start-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartMenuComponent
   ],
   imports: [
     BrowserModule
